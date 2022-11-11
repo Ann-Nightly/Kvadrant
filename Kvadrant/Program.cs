b89bc279222d77ef8c2760b444242d0edd759a39
annochnaya@gmail.com <annochnaya@gmail.com>
2022-11-12 00:24:34 +0300

    Добавьте файлы проекта.

---
+++ Kvadrant/Program.cs
@@ -0,0 +1,38 @@
+﻿
+double x, y;
+Console.WriteLine("Введите значение координаты х");
+x = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+Console.WriteLine("Введите значение координаты у");
+y = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("точка с координатами " + "(" + x + ";" + y + ") " + "находится в I квадранте");
+}
+else
+{
+    if (x < 0 && y > 0)
+    {
+        Console.WriteLine("точка с координатами " + "(" + x + ";" + y + ") " + "находится во II квадранте");
+    }
+    else
+    {
+        if (x < 0 && y < 0)
+        {
+            Console.WriteLine("точка с координатами " + "(" + x + ";" + y + ") " + "находится в III квадранте");
+        }
+        else 
+        {
+            if (x > 0 && y < 0)
+            {
+                Console.WriteLine("точка с координатами " + "(" + x + ";" + y + ") " + "находится в IV квадранте");
+            }
+            else
+            {
+            Console.WriteLine("точка с координатами " + "(" + x + ";" + y + ") " + "находится на оси ординат");
+                
+            }
+        }
+    }
+        
+}
+
